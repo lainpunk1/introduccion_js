cce639fa7201d303a75a749dc0d812b519fde4a3
Lainpunk1 <lainpunk1@protonmail.com>
2022-09-14 09:11:51 -0300

    resolucion

---
+++ main.js
@@ -0,0 +1,16 @@
+var nombre = Esteban;
+var apellido = Peña;
+var edad = 26;
+var dev = true;
+var fecha = new Date(1996, 08, 27);
+var favbook = {
+    titulo:"Pánico y locura en Las Vegas",
+    autor:"Hunter S. Thompson",
+    fecha:"11 de Noviembre de 1972",
+    ulr:"https://en.wikipedia.org/wiki/Fear_and_Loathing_in_Las_Vegas"
+}
+var pasatiempos = ["ponderar orbe", "proyeccion astral", "magia negra", "disociar"];
+function miActividadFavorita(){
+    var actividadFavorita = "ponderar orbe";
+    return actividadFavorita;
+}
